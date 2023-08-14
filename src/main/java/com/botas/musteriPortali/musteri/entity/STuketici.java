ccbe64dff64efd6bcb3c5f303ad8af9c84b53dab
Burak Kaynar <60446917+bkaynar@users.noreply.github.com>
2023-08-14 14:26:01 +0300

    Tuketici Entity Eklendi

--- src/main/java/com/botas/musteriPortali/musteri/entity/STuketici.java
+++ src/main/java/com/botas/musteriPortali/musteri/entity/STuketici.java
@@ -4,32 +4,34 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = "serbest_Tuketici")
+@Table(name = "s_tuketici")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @ToString
 @EqualsAndHashCode(of = "id")
-public class SerbestTuketici {
+public class STuketici {
     @Id
-    @SequenceGenerator(name = "seq_serbest_tuketici", initialValue = 1, allocationSize = 50)
-    @GeneratedValue(generator = "seq_serbest_tuketici", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "seq_s_tuketici", initialValue = 1, allocationSize = 50)
+    @GeneratedValue(generator = "seq_s_tuketici", strategy = GenerationType.SEQUENCE)
     private Long id;
-    @Column(name = "st_musteri_kodu")
-    public String st_musteri_kodu;
+    @Column(name = "musteri_kodu")
+    public String musteri_kodu;
+
     @Column(name = "st_adi")
     public String st_adi;
+
     @Column(name = "epdk_sektoru")
     public String epdk_sektoru;
+
     @Column(name = "hacim")
     public String hacim;
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "musteri_id")
     private Musteri musteri;
-
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "istasyon_id")
     private Istasyon istasyon;
-}
 
+}
