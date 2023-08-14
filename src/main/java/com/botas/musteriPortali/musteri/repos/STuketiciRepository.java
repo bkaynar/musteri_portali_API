b96b59b20d48fd1fa63ddf917e4a128b1c38440e
Burak Kaynar <60446917+bkaynar@users.noreply.github.com>
2023-08-14 16:16:33 +0300

    Serbest Tüketici Tanımlama

---
+++ src/main/java/com/botas/musteriPortali/musteri/repos/STuketiciRepository.java
@@ -0,0 +1,7 @@
+package com.botas.musteriPortali.musteri.repos;
+
+import com.botas.musteriPortali.musteri.entity.STuketici;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface STuketiciRepository extends JpaRepository<STuketici,Long> {
+}
---
+++ src/main/java/com/botas/musteriPortali/musteri/service/STuketiciImpl.java
@@ -0,0 +1,39 @@
+package com.botas.musteriPortali.musteri.service;
+
+import com.botas.musteriPortali.musteri.entity.STuketici;
+import com.botas.musteriPortali.musteri.repos.STuketiciRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class STuketiciImpl implements STuketiciService {
+    private final STuketiciRepository sTuketiciRepository;
+
+    public STuketiciImpl(STuketiciRepository sTuketiciRepository) {
+        this.sTuketiciRepository = sTuketiciRepository;
+    }
+
+    @Override
+    @Transactional
+    public STuketici save(STuketici sTuketici) {
+        return sTuketiciRepository.save(sTuketici);
+    }
+
+    @Override
+    public Optional<STuketici> getById(Long id) {
+        return sTuketiciRepository.findById(id);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        sTuketiciRepository.deleteById(id);
+    }
+
+    @Override
+    public List<STuketici> getAll() {
+        return sTuketiciRepository.findAll();
+    }
+}
