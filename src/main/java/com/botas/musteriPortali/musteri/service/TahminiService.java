32a6b4a83129d6a330deb528b452e52e952ff60d
Burak Kaynar <60446917+bkaynar@users.noreply.github.com>
2023-08-15 13:37:55 +0300

    Tahmini TMB Update

--- src/main/java/com/botas/musteriPortali/musteri/service/TahminiService.java
+++ src/main/java/com/botas/musteriPortali/musteri/service/TahminiService.java
@@ -2,12 +2,15 @@ package com.botas.musteriPortali.musteri.service;
 
 
 import com.botas.musteriPortali.musteri.entity.Tahmini;
+import com.botas.musteriPortali.musteri.entity.Tehis;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface TahminiService {
     Tahmini save (Tahmini tahmini);
+
+    Tahmini update(Tahmini tahmini);
 
     Optional<Tahmini> getById(Long id);
 
